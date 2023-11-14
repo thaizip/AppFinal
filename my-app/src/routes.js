cc3c96017bb310f954486b64944bab4ad727a592
Thais Oliveira <oliveirath27@gmail.com>
2023-11-14 20:58:46 -0300

    login e cadastro

--- my-app/src/routes.js
+++ my-app/src/routes.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-const Tab = createNativeStackNavigator();
-
 import Login from './screens/login';
 import Cadastro from  './screens/cadastro'; 
+
+const Menu = createNativeStackNavigator();
 
 export default function Rotas() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Login" component={Login} options={{ headerShown: false }}/>
-        <Tab.Screen name="Cadastro" component={Cadastro}  />
-      </Tab.Navigator>
+      <Menu.Navigator>
+        <Menu.Screen name="Login" component={Login} options={{ headerShown: false }}/>
+        <Menu.Screen name="Cadastro" component={Cadastro}  />
+      </Menu.Navigator>
     </NavigationContainer>
   );
 }
